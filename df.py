a451be9b7fa2bbaa1d4444cacd43bd8ac09b5ae4
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 12:26:11 +0100

    Update df.py

--- df.py
+++ df.py
@@ -5,6 +5,8 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
