ddb3dd6b510a367bd3dc1b9363112252ed58c889
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 12:34:07 +0100

    Update df.py

--- df.py
+++ df.py
@@ -38,3 +38,14 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+#map
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
