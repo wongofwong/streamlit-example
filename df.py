a41bc5e8b7653e78681b890ab52253f3be4a2cb1
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 12:36:53 +0100

    Update df.py

--- df.py
+++ df.py
@@ -50,3 +50,10 @@ st.map(map_data)
 import streamlit as st
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+#text input
+import streamlit as st
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
