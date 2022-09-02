9b44e3e0572c0632f2352096cd09cdd64ca7f920
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 12:30:52 +0100

    Update df.py

--- df.py
+++ df.py
@@ -31,3 +31,10 @@ dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 st.table(dataframe)
+
+#chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
