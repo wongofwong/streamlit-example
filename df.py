39a9686cd33afc16e04948e211fc899fe284b506
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 17:44:13 +0100

    Update df.py

--- df.py
+++ df.py
@@ -131,3 +131,11 @@ import streamlit as st
 def my_slow_function(arg1, arg2):
     # Do something really slow in here!
     return the_output
+
+#pagination
+import streamlit as st
+
+@st.cache  # 👈 This function will be cached
+def my_slow_function(arg1, arg2):
+    # Do something really slow in here!
+    return the_output
