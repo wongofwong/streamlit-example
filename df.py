52b3a14a1d271a864d14e729efc217a890c2a7bf
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 12:39:46 +0100

    Update df.py

--- df.py
+++ df.py
@@ -64,3 +64,15 @@ if st.checkbox('Show dataframe'):
        columns=['a', 'b', 'c'])
 
     chart_data
+
+#select box
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
