52853c9413b3890a40981506c0e187d82bd02246
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-02 12:27:35 +0100

    Update df.py

--- df.py
+++ df.py
@@ -16,3 +16,9 @@ df
 
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
