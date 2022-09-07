7b2242e5d7e47836966763437dc31007bc807180
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-07 17:20:25 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -36,13 +36,13 @@ components.html(
         <div class="card-header" id="headingOne">
           <h5 class="mb-0">
             <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Collapsible Group Item #1
+            Topic Model Inputs
             </button>
           </h5>
         </div>
         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
           <div class="card-body">
-            Collapsible Group Item #1 content
+            Topic Model Inputs
           </div>
         </div>
       </div>
@@ -50,13 +50,13 @@ components.html(
         <div class="card-header" id="headingTwo">
           <h5 class="mb-0">
             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Collapsible Group Item #2
+            Topic Model Output Visualizations
             </button>
           </h5>
         </div>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
           <div class="card-body">
-            Collapsible Group Item #2 content
+            Topic Model Output Visualizations
           </div>
         </div>
       </div>
