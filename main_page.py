d20e6da7307b75058fb9b4eac4585b56e3ac8e6d
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-08 00:15:28 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -98,14 +98,14 @@ components.html(
 # print(fig)
 # fig.show()
 
-# import streamlit as st
-# import plotly.figure_factory as ff
-# import numpy as np
+import streamlit as st
+import plotly.figure_factory as ff
+import numpy as np
 
-# # # Add histogram data
-# # x1 = np.random.randn(200) - 2
-# # x2 = np.random.randn(200)
-# # x3 = np.random.randn(200) + 2
+# # Add histogram data
+# x1 = np.random.randn(200) - 2
+# x2 = np.random.randn(200)
+# x3 = np.random.randn(200) + 2
 
 # # # Group data together
 # # hist_data = [x1, x2, x3]
