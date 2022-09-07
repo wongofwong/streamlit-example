35befa42e51db6d65420fd9734e3137a8f23a016
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-08 00:07:30 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -65,30 +65,30 @@ components.html(
     height=600,
 )
 
-from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
-import json
+# from dash import Dash, dcc, html, Input, Output
+# import plotly.express as px
+# import json
 
-fig = px.line(
-    x=["a","b","c"], y=[1,3,2], # replace with your own data source
-    title="sample figure", height=325
-)
+# fig = px.line(
+#     x=["a","b","c"], y=[1,3,2], # replace with your own data source
+#     title="sample figure", height=325
+# )
 
-app = Dash(__name__)
+# app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H4('Displaying figure structure as JSON'),
-    dcc.Graph(id="graph", figure=fig),
-    dcc.Clipboard(target_id="structure"),
-    html.Pre(
-        id='structure',
-        style={
-            'border': 'thin lightgrey solid', 
-            'overflowY': 'scroll',
-            'height': '275px'
-        }
-    ),
-])
+# app.layout = html.Div([
+#     html.H4('Displaying figure structure as JSON'),
+#     dcc.Graph(id="graph", figure=fig),
+#     dcc.Clipboard(target_id="structure"),
+#     html.Pre(
+#         id='structure',
+#         style={
+#             'border': 'thin lightgrey solid', 
+#             'overflowY': 'scroll',
+#             'height': '275px'
+#         }
+#     ),
+# ])
 
 # import plotly as pt
 
