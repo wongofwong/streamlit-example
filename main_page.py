a6a3fbf2cdc469488e5a4896cf9d82565152c38e
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-07 17:10:06 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,3 +2,23 @@ import streamlit as st
 
 st.markdown("# BERTopic Modeling 🎈")
 st.sidebar.markdown("#BERTopic Modeling 🎈")
+
+## layout ##
+
+#sidebar
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'BERTopic Model Approaches',
+    ('Default', 'Sentence Transformer', 'Guided Topic Modeling', 'Supervised', 'Semi-supervised')
+)
+
+add_selectbox = st.sidebar.selectbox(
+    'Dimension',
+    ('UMAP', 'PCA', 'Truncated SVD')
+)
+
+add_selectbox = st.sidebar.selectbox(
+    'Visualization',
+    ('Topics Word Scores Barchart', 'Intertopic Distance Map', 'Hierarchical Clustering', 'Similarity Heat Map', 'Topic Frequency Distribution')
+)
+
