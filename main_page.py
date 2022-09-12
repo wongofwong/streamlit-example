fe56fc96699d7f43ba69dbdc6a424d23b3167c66
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-12 17:11:11 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -25,48 +25,48 @@ add_selectbox = st.sidebar.selectbox(
 # import streamlit as st
 import streamlit.components.v1 as components
 
-# bootstrap 4 collapse example
-components.html(
-    """
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <div id="accordion">
-      <div class="card">
-        <div class="card-header" id="headingOne">
-          <h5 class="mb-0">
-            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Topic Model Custom Classes
+# # bootstrap 4 collapse example
+# components.html(
+#     """
+#     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+#     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+#     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+#     <div id="accordion">
+#       <div class="card">
+#         <div class="card-header" id="headingOne">
+#           <h5 class="mb-0">
+#             <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+#             Topic Model Custom Classes
             
        
             
-            </button>
-          </h5>
-        </div>
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-          <div class="card-body">
-            seed_topic_list=['Minestrone with beef', 'Beef chili', 'Oatmeal with flax seeds and apple', 'Blueberry shortcake', 'Roast beef sandwich', 'Turkey Sandwich', 'Classic Caesar salad', 'Mixed berry shortcake', 'Veggie sandwich', 'Classic Caesar salad with edamame', 'Oatmeal with diced apple', 'Classic minestrone with leafy greens', 'Chocolate chip cookies with pecans', 'Lasagna with meat sauce', 'Strawberry shortcake', 'Vegetarian pizza', 'Chocolate chip cookies', 'Oatmeal with diced apple and pistachios', 'Classic Caesar salad with chicken', 'Vegetarian lasagna with spinach', 'Classic minestrone', 'Chicken and bean chili', 'Vegetarian lasagna', 'Beef Steak', 'Tilapia', 'Banana', 'Green beans', 'pecans', 'Baked Beans', 'Cherries', 'Macaroni and cheese', 'brown rice', 'Brownie ', 'White Bread', 'Carrots', 'green peas', 'cornbread', 'Potato', 'Canned pear halves', 'Corn', 'String cheese', 'Fried Chicken Breast', 'Fried eggs', 'Peanut Butter', 'Pork roast', 'chicken patty', 'Pear ', 'tapioca pudding, vanilla and other flavors, homemade', 'rice pudding, plain, made with egg', 'plain yogurt, lowfat', 'oatmeal, regular cooking', 'macaroni noodles', 'angel hair pasta', 'chocolate pudding, from mix, instant', 'garbanzo beans', 'cottage cheese']
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="headingTwo">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Topic Model Output Visualizations
-            </button>
-          </h5>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-          <div class="card-body">
-            Topic Model Output Visualizations
-          </div>
-        </div>
-      </div>
-    </div>
-    """,
-    height=600,
-)
+#             </button>
+#           </h5>
+#         </div>
+#         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+#           <div class="card-body">
+#             seed_topic_list=['Minestrone with beef', 'Beef chili', 'Oatmeal with flax seeds and apple', 'Blueberry shortcake', 'Roast beef sandwich', 'Turkey Sandwich', 'Classic Caesar salad', 'Mixed berry shortcake', 'Veggie sandwich', 'Classic Caesar salad with edamame', 'Oatmeal with diced apple', 'Classic minestrone with leafy greens', 'Chocolate chip cookies with pecans', 'Lasagna with meat sauce', 'Strawberry shortcake', 'Vegetarian pizza', 'Chocolate chip cookies', 'Oatmeal with diced apple and pistachios', 'Classic Caesar salad with chicken', 'Vegetarian lasagna with spinach', 'Classic minestrone', 'Chicken and bean chili', 'Vegetarian lasagna', 'Beef Steak', 'Tilapia', 'Banana', 'Green beans', 'pecans', 'Baked Beans', 'Cherries', 'Macaroni and cheese', 'brown rice', 'Brownie ', 'White Bread', 'Carrots', 'green peas', 'cornbread', 'Potato', 'Canned pear halves', 'Corn', 'String cheese', 'Fried Chicken Breast', 'Fried eggs', 'Peanut Butter', 'Pork roast', 'chicken patty', 'Pear ', 'tapioca pudding, vanilla and other flavors, homemade', 'rice pudding, plain, made with egg', 'plain yogurt, lowfat', 'oatmeal, regular cooking', 'macaroni noodles', 'angel hair pasta', 'chocolate pudding, from mix, instant', 'garbanzo beans', 'cottage cheese']
+#           </div>
+#         </div>
+#       </div>
+#       <div class="card">
+#         <div class="card-header" id="headingTwo">
+#           <h5 class="mb-0">
+#             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+#             Topic Model Output Visualizations
+#             </button>
+#           </h5>
+#         </div>
+#         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+#           <div class="card-body">
+#             Topic Model Output Visualizations
+#           </div>
+#         </div>
+#       </div>
+#     </div>
+#     """,
+#     height=600,
+# )
 
 from bertopic import BERTopic
 import pandas as pd
