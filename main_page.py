7c20a211d1b2ddc238a316d1ce3972ac6065c665
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-12 19:08:17 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -27,8 +27,12 @@ viz_options = ['Topics Word Scores Barchart', 'Intertopic Distance Map', 'Hierar
 viz_selected = st.sidebar.selectbox('Visualization', options = viz_options)
 
 if viz_selected == 'Topics Word Scores Barchart':
-    st.markdown("[Section 1: Section 1: Topics Barchart](#section-1)")
-
+    st.markdown("[Section 1: Topics Barchart](#section-1)")
+elif viz_selected == 'Intertopic Distance Map':
+    st.markdown("[Section 2: Topics Clusters](#section-2)")
+else viz_selected == 'Hierarchical Clustering':   
+    st.markdown("[Section 3: Hierarchical Clusters](#section-3)")
+    
 # import streamlit as st
 import streamlit.components.v1 as components
 
