87c7fd19453173b0e2c0faba49f5642ff8e41921
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2022-09-13 16:14:29 +0100

    Update topic_model.py

--- topic_model.py
+++ topic_model.py
@@ -152,15 +152,33 @@ import streamlit as st
 import plotly.figure_factory as ff
 import numpy as np
 
-st.header("Section 1: Topics Barchart")
+st.header("Topics Barchart")
 st.plotly_chart(fig_barchart, use_container_width=True)
 
-st.header("Section 2: Topics Clusters")
+st.header("Topics Clusters")
 st.plotly_chart(fig_docs_clusters, use_container_width=True)
 
-st.header("Section 3: Hierarchical Clusters")
+st.header("Hierarchical Clusters")
 st.plotly_chart(fig_hier, use_container_width=True)
 # st.plotly_chart(fig_term_rank, use_container_width=True)
 # st.plotly_chart(fig_heatmap, use_container_width=True)
 # st.plotly_chart(fig_topics, use_container_width=True)
 # st.plotly_chart(fig_freq, use_container_width=True)
+
+import streamlit as st
+from st_clickable_images import clickable_images
+
+clicked = clickable_images(
+    [
+        "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
+        "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
+        "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
+        "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
+        "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
+    ],
+    titles=[f"Image #{str(i)}" for i in range(5)],
+    div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
+    img_style={"margin": "5px", "height": "200px"},
+)
+
+st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
