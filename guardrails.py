c75eb3f95221726f889c1b1ae6b786ff36770e46
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2025-04-02 14:11:52 -0700

    Update guardrails.py

--- guardrails.py
+++ guardrails.py
@@ -41,9 +41,9 @@ if uploaded_file is not None:
 #         # from different file types.
 
 #        
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+        # # To read file as bytes:
+        # bytes_data = uploaded_file.getvalue()
+        # st.write(bytes_data)
     
         # To convert to a string based IO:
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
