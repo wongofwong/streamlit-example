df26567a117d3c50f9de5e163c95aae6661a4f2c
Jimmy Wong <jimmy.t.k.wong@gmail.com>
2025-04-02 14:14:18 -0700

    Update guardrails.py

--- guardrails.py
+++ guardrails.py
@@ -41,23 +41,25 @@ if uploaded_file is not None:
 #         # from different file types.
 
 #        
-        # # To read file as bytes:
-        # bytes_data = uploaded_file.getvalue()
-        # st.write(bytes_data)
+        # # # To read file as bytes:
+        # # bytes_data = uploaded_file.getvalue()
+        # # st.write(bytes_data)
     
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
+        # # To convert to a string based IO:
+        # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        # st.write(stringio)
     
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
+        # # To read file as string:
+        # string_data = stringio.read()
+        # st.write(string_data)
     
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
-        file_contents = uploaded_file.read().decode("utf-8")  # Assuming text-based file for now
-
+        # # Can be used wherever a "file-like" object is accepted:
+        # dataframe = pd.read_csv(uploaded_file)
+        # st.write(dataframe)
+        # file_contents = uploaded_file.read().decode("utf-8")  # Assuming text-based file for now
+        
+        #Read
+        file_contents = uploaded_file.read().decode() 
 
         # Apply guardrails
         compliance_results = brand_compliance_guardrails(file_contents)
